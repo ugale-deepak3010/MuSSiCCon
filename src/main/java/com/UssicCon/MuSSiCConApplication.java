50227d14d4219928732c7410bec0a612166b3783
Dipak Ugale <ugale.deepak3010@gmail.com>
2025-01-19 13:47:44 +0530

    minor changes

--- src/main/java/com/UssicCon/MuSSiCConApplication.java
+++ src/main/java/com/UssicCon/MuSSiCConApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UssicConApplication {
+public class MuSSiCConApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UssicConApplication.class, args);
+        SpringApplication.run(MuSSiCConApplication.class, args);
     }
 
 }
